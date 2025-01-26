a9bc301c95971f80c0715228787ecb3a6aec3733
turccdeter <dsa@mtb111.fr>
2025-01-26 22:11:14 +0100

    changement de index;js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ShopContextProvider from './Context/ShopContext';
 
+// Création du root pour le rendu de React
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Rendu de l'application React
 root.render(
-  <ShopContextProvider>
-    <App />
-  </ShopContextProvider>
+  <React.StrictMode>
+    <App /> {/* Ton App.js gère déjà ShopContextProvider et HashRouter */}
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Mesure des performances
 reportWebVitals();
